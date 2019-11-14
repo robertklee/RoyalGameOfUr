57325ad4cb51651062e2921ffef51c9f33d6cd3c
Robert Lee <32447910+robertklee@users.noreply.github.com>
2019-11-13 20:22:49 -0800

    implemented cookie and generation of unique UUID

--- jsx/Ur.jsx
+++ jsx/Ur.jsx
@@ -100,6 +100,9 @@ render() {
 }
 }
 
+var cookieName = "UrGameCookie";
+var cookieDays = 14;
+
 class Game extends React.Component {
 
 constructor(props) {
@@ -107,10 +110,18 @@ constructor(props) {
   let r = generateId().substring(0,keyLength);
   console.log("Game Key: ", r);
   super(props);
+
+  var cookieValue = readCookie(cookieName)
+  if (!cookieValue) {
+    cookieValue = uuidv4();
+    createCookie(cookieName, cookieValue, cookieDays);
+  }
+  
   this.state = {
     gameKey: r.toUpperCase(),
     currentDisplay: Array(40).fill(""),
-    diceRoll: -1
+    diceRoll: -1,
+    cookie: cookieValue,
   };
 
   this.handleGameKeyChange = this.handleGameKeyChange.bind(this);
@@ -121,7 +132,8 @@ componentDidMount() {
   this.intervalID = setInterval(
     () => sendDataToServer(this, {
       clickPosition: -1,
-      game_key: this.state.gameKey
+      game_key: this.state.gameKey,
+      cookie: this.state.cookie
     }),
     1000
   );
@@ -142,6 +154,7 @@ handleClick(i) {
   var data = {
     clickPosition: i,
     game_key: this.state.gameKey,
+    cookie: this.state.cookie,
   };
 
   sendDataToServer(this, data);
@@ -166,6 +179,7 @@ handleGameKeySubmit(event) {
   var data = {
     clickPosition: -1,
     game_key: this.state.gameKey,
+    cookie: this.state.cookie,
   };
 
   sendDataToServer(this, data)
@@ -239,6 +253,40 @@ function sendDataToServer(targetobject, data) {
   request.send(JSON.stringify(data));
 }
 
+// credit to https://www.quirksmode.org/js/cookies.html
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name) {
+	createCookie(name,"",-1);
+}
+
+// Credit to https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+function uuidv4() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
+
+// Credit to https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript/8084248#8084248
 // dec2hex :: Integer -> String
 // i.e. 0-255 -> '00'-'ff'
 function dec2hex (dec) {
