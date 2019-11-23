2eaebf24f6c25e54ea1fabf0658b5054536bb6ec
Robert Lee <32447910+robertklee@users.noreply.github.com>
2019-11-22 20:05:37 -0800

    added file to display database stats

---
+++ database_stats.py
@@ -0,0 +1,39 @@
+import sqlite3
+from datetime import datetime
+from urllib.request import pathname2url
+
+dbFileName = "Logs.db"
+connectionTableName = "ConnectionLogs"
+
+
+# Initialize Database
+# Credit to https://stackoverflow.com/questions/12932607/how-to-check-if-a-sqlite3-database-exists-in-python
+conn = None
+try:
+    dburi = 'file:{}?mode=rw'.format(pathname2url(dbFileName))
+    conn = sqlite3.connect(dburi, uri=True)
+except sqlite3.OperationalError:
+    # handle missing database case
+    quit()
+
+curs = conn.cursor()
+# curs.execute("DROP TABLE " + connectionTableName) # TODO debug only
+curs.execute("CREATE TABLE IF NOT EXISTS " + connectionTableName + " (cookie STRING PRIMARY KEY, firstAccessDate BIGINT, numValidMoves INT);")
+conn.commit()
+
+cookie = 'xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx'
+
+# curs.execute("INSERT INTO " + connectionTableName + " (cookie, firstAccessDate, numValidMoves) VALUES (?,?,?)", (cookie, datetime.today().timestamp(), 3))
+# curs.execute("UPDATE " + connectionTableName + " SET numValidMoves=numValidMoves+1 WHERE cookie=?", (cookie,))
+# curs.execute("SELECT COUNT(*) FROM " + connectionTableName + " WHERE cookie=?;", (cookie,))
+
+curs.execute("SELECT COUNT(*) FROM " + connectionTableName)
+count = curs.fetchone()
+
+print("Stats for Table \"%s\"\n\n" % connectionTableName)
+print("Executing SQL Query \" %s \"..." % ("SELECT * FROM " + connectionTableName))
+print("Number of rows is: " + str(count[0]))
+for row in curs.execute("SELECT * FROM " + connectionTableName):
+    print(row)
+
+conn.commit()
